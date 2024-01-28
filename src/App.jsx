cbdcc8b6dba08dc5b8ebfe57657251d3b9a1bef3
danimisme <msubhanr53@gmail.com>
2024-01-28 13:29:29 +0700

    installasi vite js

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <div className="flex justify-center bg-blue-500 min-h-screen items-center">
+      <div>Hello World</div>
+    </div>
+  )
+}
+
+export default App
