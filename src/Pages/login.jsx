73060eaee2abc5574d59fc2727e092472ac05886
danimisme <msubhanr53@gmail.com>
2024-01-28 16:21:54 +0700

    meggunakan link untuk berpindah halaman

--- src/Pages/login.jsx
+++ src/Pages/login.jsx
@@ -1,11 +1,18 @@
-import AuthLayout from "../componnents/Layouts/AuthLayouts"
-import FormLogin from "../componnents/Fragments/FormLogin"
+import AuthLayout from "../componnents/Layouts/AuthLayouts";
+import FormLogin from "../componnents/Fragments/FormLogin";
+import { Link } from "react-router-dom";
 const LoginPage = () => {
-    return (
-        <AuthLayout title="Login">
-            <FormLogin />
-        </AuthLayout>
-    )
-}
+  return (
+    <AuthLayout title="Login">
+      <FormLogin />
+      <p className="text-sm mt-5 text-center">
+        Don't have an account?
+        <Link to="/register" className="text-blue-600 font-bold">
+          Register
+        </Link>
+      </p>
+    </AuthLayout>
+  );
+};
 
-export default LoginPage
+export default LoginPage;
--- src/Pages/register.jsx
+++ src/Pages/register.jsx
@@ -1,11 +1,18 @@
-import AuthLayout from "../componnents/Layouts/AuthLayouts"
-import FormRegister from "../componnents/Fragments/FormRegister"
+import AuthLayout from "../componnents/Layouts/AuthLayouts";
+import FormRegister from "../componnents/Fragments/FormRegister";
+import { Link } from "react-router-dom";
 const RegisterPage = () => {
-    return (
-        <AuthLayout title="Register">
-            <FormRegister />
-        </AuthLayout>
-    )
-}
+  return (
+    <AuthLayout title="Register">
+      <FormRegister />
+      <p className="text-sm mt-5 text-center">
+        Have an account?
+        <Link to="/login" className="text-blue-600 font-bold">
+          Login
+        </Link>
+      </p>
+    </AuthLayout>
+  );
+};
 
-export default RegisterPage
+export default RegisterPage;
