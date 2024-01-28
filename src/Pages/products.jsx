8c5c3b32ecd6dda6d217abc6d409bd3c995106d8
danimisme <msubhanr53@gmail.com>
2024-01-28 18:14:03 +0700

    menerapkan rendering list pada ProductPages

--- src/Pages/products.jsx
+++ src/Pages/products.jsx
@@ -1,18 +1,43 @@
 import CardProduct from "../componnents/Fragments/CardProduct";
 
+const products = [
+  {
+    id: 1,
+    name: "Sepatu Baru",
+    price: "Rp. 2000.000",
+    image: "/images/shoes-1.jpg",
+    description:
+      "hehe Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae lorem lorem lorem  lorem   ",
+  },
+  {
+    id: 2,
+    name: "Sepatu Lama",
+    price: "Rp. 1000.000",
+    image: "/images/shoes-1.jpg",
+    description:
+      "hehe lorem ipsum dolor sit amet consectetur adipisicing elit.",
+  },
+  {
+    id: 3,
+    name: "Sepatu Adadong",
+    price: "Rp. 2000.000",
+    image: "/images/shoes-1.jpg",
+    description: "ini adalah sepatu baru dari brand adadong.",
+  },
+];
+
 const ProductsPage = () => {
   return (
     <div className="flex justify-center py-5">
-      <CardProduct>
-        <CardProduct.Header image="/images/shoes-1.jpg" />
-        <CardProduct.Body title="Sepatu Baru">
-          hehe Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae
-          temporibus molestiae, impedit voluptas sunt perferendis voluptatum
-          fuga vero quibusdam dolore. Est consectetur ullam ratione reiciendis
-          itaque vel, placeat rerum in?
-        </CardProduct.Body>
-        <CardProduct.Footer price="Rp. 2000.000" />
-      </CardProduct>
+      {products.map((product) => (
+        <CardProduct key={product.id}>
+          <CardProduct.Header image={product.image} />
+          <CardProduct.Body name={product.name}>
+            {product.description}
+          </CardProduct.Body>
+          <CardProduct.Footer price={product.price} />
+        </CardProduct>
+      ))}
     </div>
   );
 };
