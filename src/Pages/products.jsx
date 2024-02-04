312a4707ee2dd2fb8451384a5eb2a9a967e169be
danimisme <msubhanr53@gmail.com>
2024-02-04 15:50:44 +0700

    belajar menerapkan redux untuk mengelola state Cart

--- src/Pages/products.jsx
+++ src/Pages/products.jsx
@@ -1,18 +1,13 @@
-import { Fragment, useEffect, useRef, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 import CardProduct from "../componnents/Fragments/CardProduct";
 import Button from "../componnents/Elements/Button";
 import { getProducts } from "../services/product.service";
 import useLogin from "../hooks/useLogin";
+import TableCart from "../componnents/Fragments/TableCart";
+import Navbar from "../componnents/Layouts/Navbar";
 const ProductsPage = () => {
-  const [cart, setCart] = useState([]);
-  const [totalPrice, setTotalPrice] = useState(0);
   const [products, setProducts] = useState([]);
-  const username = useLogin();
-
-  useEffect(() => {
-    const data = JSON.parse(localStorage.getItem("cart")) || [];
-    setCart(data);
-  }, []);
+  useLogin();
 
   useEffect(() => {
     getProducts((data) => {
@@ -20,60 +15,14 @@ const ProductsPage = () => {
     });
   }, []);
 
-  useEffect(() => {
-    if (products.length > 0 && cart.length > 0) {
-      const sum = cart.reduce((acc, item) => {
-        const product = products.find((product) => product.id === item.id);
-        return acc + product.price * item.qty;
-      }, 0);
-      localStorage.setItem("cart", JSON.stringify(cart));
-      setTotalPrice(sum);
-    }
-  }, [cart, products]);
-
   const handleLogout = () => {
     localStorage.removeItem("token");
     window.location.href = "/login";
   };
 
-  const handleAddToCart = (id) => {
-    if (cart.find((product) => product.id === id)) {
-      setCart(
-        cart.map((item) =>
-          item.id === id ? { ...item, qty: item.qty + 1 } : item
-        )
-      );
-    } else {
-      setCart([...cart, { id, qty: 1 }]);
-    }
-  };
-
-  //useRef
-  const cartRef = useRef(JSON.parse(localStorage.getItem("cart")) || []);
-
-  const handleAddToCartRef = (id) => {
-    cartRef.current = [...cartRef.current, { id, qty: 1 }];
-    localStorage.setItem("cart", JSON.stringify(cartRef.current));
-  };
-
-  const totalPriceRef = useRef(null);
-
-  useEffect(() => {
-    if (cart.length > 0) {
-      totalPriceRef.current.style.display = "table-row";
-    } else {
-      totalPriceRef.current.style.display = "none";
-    }
-  }, [cart]);
-
   return (
     <Fragment>
-      <div className="flex justify-end h-20 bg-blue-600 text-white items-center px-10">
-        {username}{" "}
-        <Button classname="ml-5 bg-red-600" onClick={handleLogout}>
-          Logout
-        </Button>
-      </div>
+      <Navbar />
       <div className="flex justify-center py-5">
         <div className="w-2/3 flex flex-wrap">
           {products.length > 0 &&
@@ -83,69 +32,13 @@ const ProductsPage = () => {
                 <CardProduct.Body name={product.title}>
                   {product.description}
                 </CardProduct.Body>
-                <CardProduct.Footer
-                  price={product.price}
-                  id={product.id}
-                  handleAddToCart={handleAddToCart}
-                />
+                <CardProduct.Footer price={product.price} id={product.id} />
               </CardProduct>
             ))}
         </div>
         <div className="w-1/3">
           <h1 className="text-3xl font-bold text-blue-600 ml-5 mb-2">Cart</h1>
-          <table className="text-left table-auto border-separate border-spacing-x-5">
-            <thead>
-              <tr>
-                <th>Product</th>
-                <th>Price</th>
-                <th>Quantity</th>
-                <th>Total</th>
-              </tr>
-            </thead>
-            <tbody>
-              {products.length > 0 &&
-                cart.map((item) => {
-                  const product = products.find(
-                    (product) => product.id === item.id
-                  );
-                  return (
-                    <tr key={item.id}>
-                      <td>{product.title.substring(0, 10)}...</td>
-                      <td>
-                        ${" "}
-                        {product.price.toLocaleString("id-ID", {
-                          styles: "currency",
-                          currency: "USD",
-                        })}
-                      </td>
-                      <td>{item.qty}</td>
-                      <td>
-                        ${" "}
-                        {(product.price * item.qty).toLocaleString("id-ID", {
-                          styles: "currency",
-                          currency: "USD",
-                        })}
-                      </td>
-                    </tr>
-                  );
-                })}
-              <tr ref={totalPriceRef}>
-                <td colSpan={3}>
-                  {" "}
-                  <b> Total Price</b>
-                </td>
-                <td>
-                  <b>
-                    ${" "}
-                    {totalPrice.toLocaleString("id-ID", {
-                      styles: "currency",
-                      currency: "USD",
-                    })}
-                  </b>
-                </td>
-              </tr>
-            </tbody>
-          </table>
+          <TableCart products={products} />
         </div>
       </div>
     </Fragment>
--- src/redux/slices/cartSlice.js
+++ src/redux/slices/cartSlice.js
@@ -3,19 +3,38 @@ import { createSlice } from "@reduxjs/toolkit";
 const cartSlice = createSlice({
   name: "cart",
   initialState: {
-    data: [
-      {
-        id: 1,
-        qty: 1,
-      },
-    ],
+    data: JSON.parse(localStorage.getItem("cart")) || [],
   },
   reducers: {
-    addToCart(state, action) {
-      state.data.push(action.payload);
+    addToCart: (state, action) => {
+      const itemInCart = state.data.find(
+        (item) => item.id === action.payload.id
+      );
+      if (itemInCart) {
+        itemInCart.qty++;
+      } else {
+        state.data.push(action.payload);
+      }
     },
   },
 });
+
+// const cartSlice = createSlice({
+//   name: "cart",
+//   initialState: {
+//     data: [
+//       {
+//         id: 1,
+//         qty: 1,
+//       },
+//     ],
+//   },
+//   reducers: {
+//     addToCart(state, action) {
+//       state.data.push(action.payload);
+//     },
+//   },
+// });
 
 export const { addToCart } = cartSlice.actions;
 export default cartSlice.reducer;
